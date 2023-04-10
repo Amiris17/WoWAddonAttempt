1c988cee085c45d0c7a8d27fceb1c6148c984ab7
Mir <95391693+Amiris17@users.noreply.github.com>
2023-04-10 15:31:31 -0400

    Create HydrationCheck.lua

---
+++ HydrationCheck.lua
@@ -0,0 +1,31 @@
+global y=0
+global n=0
+
+PopupDialog["HYDRATION_CHECK"]={
+    text="Have you drank water in the past hour?",
+    button1="Yes",
+    button2="No",
+    onAccept=Function()
+
+}
+
+
+local spell_name=getspellinfo
+local concshot_count=0
+local best_wrath=0
+if (spell_name) tostring=="Concussive Shot" then
+    concshot_count=concshot_count+1
+
+if (spell_name) tostring=="5116" --Incase it is a numeric value instead.
+    concshot_count=concshot_count+1
+
+if (spell_name) tostring=="Concussive Shot" then
+    best_wrath=best_wrath+1
+    
+if (spell_name) tostring=="5116" --Incase it is a numeric value instead.
+    best_wrath=best_wrath+1
+
+
+function reminder_water(y,n):
+    if n>y:
+    /script message("DRINK WATER NOW!")
